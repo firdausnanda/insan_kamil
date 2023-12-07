eda76b243f4acf33a4897f4468998a160ce6f4bf
firdausnanda <firdausnanda46@gmail.com>
2023-12-07 14:23:26 +0700

    fix:migration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Keranjang;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
+
         view()->composer(['layouts.landing.header'], function($view) {
             $view->with('cart', Keranjang::with('produk')->where('id_user', Auth::user()->id ?? 1)->count());
         });
