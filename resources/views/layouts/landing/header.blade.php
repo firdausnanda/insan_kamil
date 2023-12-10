12ced1a1c450b079574c3179b3cf38777ac0a0bc
firdausnanda <firdausnanda46@gmail.com>
2023-12-10 21:38:10 +0700

    fix: responsive menu

--- resources/views/layouts/landing/header.blade.php
+++ resources/views/layouts/landing/header.blade.php
@@ -29,38 +29,115 @@
                         <div class="d-flex align-items-center lh-1">
 
                             <div class="list-inline me-4">
-                                <div class="list-inline-item">
+                                @guest
+                                    <div class="list-inline-item me-5">
+                                        <a href="{{ route('login') }}" class="text-muted">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                                stroke-linecap="round" stroke-linejoin="round" class="feather feather-user">
+                                                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                                                <circle cx="12" cy="7" r="4"></circle>
+                                            </svg>
+                                        </a>
+                                    </div>
 
-                                    <a href="#!" class="text-muted" data-bs-toggle="modal"
-                                        data-bs-target="#userModal">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                            stroke-linecap="round" stroke-linejoin="round" class="feather feather-user">
-                                            <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-                                            <circle cx="12" cy="7" r="4"></circle>
-                                        </svg>
-                                    </a>
-                                </div>
-                                <div class="list-inline-item">
+                                @endguest
+                                @auth
+                                    @if (Auth::user()->roles[0]->name == 'user')
+                                        {{-- Button Keranjang --}}
+                                        <div class="list-inline-item mx-5">
 
-                                    <a class="text-muted position-relative" href="{{ route('user.keranjang.index') }}">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                            stroke-linecap="round" stroke-linejoin="round"
-                                            class="feather feather-shopping-bag">
-                                            <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path>
-                                            <line x1="3" y1="6" x2="21" y2="6"></line>
-                                            <path d="M16 10a4 4 0 0 1-8 0"></path>
-                                        </svg>
-                                        @if ($cart >= 1)
-                                            <span
-                                                class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success">
-                                                {{ $cart }}
-                                                <span class="visually-hidden">unread messages</span>
-                                            </span>
-                                        @endif
-                                    </a>
-                                </div>
+                                            <a class="text-muted position-relative "
+                                                href="{{ route('user.keranjang.index') }}">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                    class="feather feather-shopping-bag">
+                                                    <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path>
+                                                    <line x1="3" y1="6" x2="21" y2="6">
+                                                    </line>
+                                                    <path d="M16 10a4 4 0 0 1-8 0"></path>
+                                                </svg>
+                                                @if ($cart >= 1)
+                                                    <span
+                                                        class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success">
+                                                        {{ $cart }}
+                                                        <span class="visually-hidden">unread messages</span>
+                                                    </span>
+                                                @endif
+                                            </a>
+
+                                        </div>
+
+                                        {{-- Button Icon Profil --}}
+                                        <div class="list-inline-item">
+                                            <a href="#" role="button" data-bs-toggle="dropdown"
+                                                aria-expanded="false">
+                                                @if (Auth::user()->avatar == null)
+                                                    <img src="{{ asset('images/avatar/user.png') }}" alt=""
+                                                        class="avatar avatar-md rounded-circle">
+                                                @else
+                                                    <img src="{{ Auth::user()->avatar }}" alt=""
+                                                        class="avatar avatar-md rounded-circle">
+                                                @endif
+                                            </a>
+
+                                            <div class="dropdown-menu dropdown-menu-end p-0">
+
+                                                <div class="lh-1 px-5 py-4 border-bottom">
+                                                    <h5 class="mb-1 h6">{{ Auth::user()->name }}</h5>
+                                                    <small>{{ Auth::user()->email }}</small>
+                                                </div>
+
+                                                <ul class="list-unstyled px-2 py-3">
+                                                    <li>
+                                                        <a class="dropdown-item" href="{{ route('user.profile.index') }}">
+                                                            Profile
+                                                        </a>
+                                                    </li>
+                                                    <li>
+                                                        <a class="dropdown-item"
+                                                            href="{{ route('user.order.konfirmasi') }}">
+                                                            Pesanan
+                                                        </a>
+                                                    </li>
+                                                    <li>
+                                                        <a class="dropdown-item"
+                                                            href="{{ route('user.order.konfirmasi') }}">
+                                                            Riwayat Transaksi
+                                                        </a>
+                                                    </li>
+                                                </ul>
+                                                <div class="border-top px-5 py-3">
+                                                    <a class="d-block" href="{{ route('logout') }}"
+                                                        onclick="event.preventDefault();
+                                                      document.getElementById('logout-form').submit();">
+                                                        Logout
+                                                    </a>
+
+                                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                                        class="d-none">
+                                                        @csrf
+                                                    </form>
+                                                </div>
+
+
+
+                                            </div>
+                                        </div>
+                                    @elseif (Auth::user()->roles[0]->name == 'admin')
+                                        <a href="{{ route('login') }}" role="button">
+                                            @if (Auth::user()->avatar == null)
+                                                <img src="{{ asset('images/avatar/user.png') }}" alt=""
+                                                    class="avatar avatar-md rounded-circle">
+                                            @else
+                                                <img src="{{ Auth::user()->avatar }}" alt=""
+                                                    class="avatar avatar-md rounded-circle">
+                                            @endif
+                                        </a>
+                                    @endif
+
+                                @endauth
 
                             </div>
                         </div>
@@ -77,7 +154,8 @@
                                     type="button">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                                         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                        stroke-linecap="round" stroke-linejoin="round" class="feather feather-search">
+                                        stroke-linecap="round" stroke-linejoin="round"
+                                        class="feather feather-search">
                                         <circle cx="11" cy="11" r="8"></circle>
                                         <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
                                     </svg>
