c1e5c405a019c221ef5036254ee1c9767aed2650
firdausnanda <firdausnanda46@gmail.com>
2024-02-27 09:45:30 +0700

    fix: landing

--- resources/views/pages/landing/index.blade.php
+++ resources/views/pages/landing/index.blade.php
@@ -259,9 +259,9 @@
                                         <div class="mt-6 mb-6">
                                             <div class="d-flex justify-content-between mb-2">
                                                 <span>Terjual: <span
-                                                        class="text-dark fs-6 fw-bold">{{ $promo->stok->sisa_produk }}</span></span>
+                                                        class="text-dark fs-6 fw-bold">{{ $promo->stok->jumlah_produk - $promo->stok->sisa_produk }}</span></span>
                                                 <span>Tersedia: <span
-                                                        class="text-dark fs-6 fw-bold">{{ $promo->stok->jumlah_produk }}</span></span>
+                                                        class="text-dark fs-6 fw-bold">{{ $promo->stok->sisa_produk }}</span></span>
                                             </div>
 
                                             <div class="progress bg-light-danger" role="progressbar"
