ee4a083d5e08110e08c11f208c3b66adf8787f31
firdausnanda <firdausnanda46@gmail.com>
2024-03-23 12:26:06 +0700

    fix: dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -10,6 +10,9 @@ use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+use function PHPSTORM_META\map;
 
 class DashboardController extends Controller
 {
@@ -20,10 +23,13 @@ class DashboardController extends Controller
         // Tanggal awal dan akhir bulan ini
         $startDate = Carbon::now()->startOfMonth();
         $endDate = Carbon::now()->endOfMonth();
+        $awalTahun = Carbon::now()->startOfYear();
+        $akhirTahun = Carbon::now()->endOfYear();
+
+        $transaksi = Pembayaran::where('status_pembayaran', 2)->get();
 
         // Total Transaksi 1 Bulan Terakhir
-        $total_transaksi = Pembayaran::where('status_pembayaran', 2)
-                                ->whereBetween('updated_at', [$startDate, $endDate])
+        $total_transaksi = $transaksi->whereBetween('updated_at', [$startDate, $endDate])
                                 ->sum('harga_jual');
 
         // Total Order 1 bulan terakhir
@@ -36,14 +42,97 @@ class DashboardController extends Controller
         })->get();
         $customer_bulan_ini = $customer->whereBetween('created_at', [$startDate, $endDate])->count();
 
-        // Table Transaksi terbaru
+        // Persentase Pendapatan
+        $pendapatan_tahun_lalu = $transaksi
+                                    ->whereBetween('created_at', [$awalTahun->subYear(), $akhirTahun->subYear()])
+                                    ->sum('harga_jual');
+                                    
+        $pendapatan_tahun_ini = $transaksi
+                                    ->whereBetween('created_at', [$awalTahun, $akhirTahun])
+                                    ->sum('harga_jual');
+        
+        if ($pendapatan_tahun_ini > $pendapatan_tahun_lalu) {
+            // persentase keuntungan
+            $persentase_transaksi[] = ($pendapatan_tahun_ini - $pendapatan_tahun_lalu) / $pendapatan_tahun_ini * 100;
+            $persentase_transaksi[] = '+';
+        }elseif ($pendapatan_tahun_ini < $pendapatan_tahun_lalu) {
+            // persentase kerugian
+            $persentase_transaksi[] = $pendapatan_tahun_lalu - $pendapatan_tahun_ini / $pendapatan_tahun_ini * 100;
+            $persentase_transaksi[] = '-';
+        }else{
+            $persentase_transaksi[] = 0;
+            $persentase_transaksi[] = '';
+        }
+
         if ($request->ajax()) {
+            
+            if ($request->chart == 1) {
+                
+                // Bulan
+                $transaksi_bulan = Pembayaran::select(DB::raw('MONTH(created_at) as bulan'), DB::raw('SUM(harga_jual) as total'))
+                                    ->whereBetween('created_at', [$awalTahun->subYear(), Carbon::now()])
+                                    ->groupBy(DB::raw('MONTH(created_at)'))
+                                    ->get();
+
+                $data = $transaksi_bulan->map(function ($item, $key) {
+                    
+                    $tanggal = Carbon::create(null, $item->bulan, 1);
+                    $namaBulan = $tanggal->locale('id')->isoFormat('MMMM');
+
+                    return [
+                        'bulan' => $namaBulan, 
+                        'total' => floor($item->total / 1000) 
+                    ];
+                });
+
+                return ResponseFormatter::success($data, 'Data berhasil diambil');
+            }elseif ($request->chart == 2) {
+                
+                $order = Order::select('status', DB::raw('COUNT(status) as total'))
+                                    ->whereBetween('created_at', [$awalTahun->subYear(), Carbon::now()])
+                                    ->where('status', '>=', 2)
+                                    ->orderBy('status')
+                                    ->groupBy('status')
+                                    ->get();
+
+                $data = $order->map(function ($item, $key) {
+                    
+                    switch ($item->status) {
+                        case 2:
+                            $status = 'Order';
+                            break;
+                        case 3:
+                            $status = 'Pengemasan';
+                            break;
+                        case 4:
+                            $status = 'Pengiriman';
+                            break;
+                        case 5:
+                            $status = 'Selesai';
+                            break;
+                            
+                        default:
+                            $status = 'Belum Dibayar';
+                            break;
+                    }
+
+                    return [
+                        'status' => $status, 
+                        'total' => $item->total 
+                    ];
+                });
+
+                return ResponseFormatter::success($data, 'Data berhasil diambil');
+            }
+
+            // Table Transaksi terbaru
             $transaksi_terakhir = $jumlah_order->whereBetween('created_at', [$startDate, $endDate]);
             return ResponseFormatter::success($transaksi_terakhir, 'Data berhasil diambil');
         }
 
         return view('pages.admin.dashboard.index', compact('quote', 'total_transaksi',
                                                     'jumlah_order', 'jumlah_order_bulan_ini',
-                                                    'customer', 'customer_bulan_ini'));
+                                                    'customer', 'customer_bulan_ini',
+                                                    'persentase_transaksi'));
     }
 }
--- resources/views/pages/admin/dashboard/index.blade.php
+++ resources/views/pages/admin/dashboard/index.blade.php
@@ -102,16 +102,17 @@
                             <div class="d-flex justify-content-between">
                                 <div>
                                     <h3 class="mb-1 fs-5">Pendapatan </h3>
-                                    <small>(+63%) than last year)</small>
+                                    <small>({{ $persentase_transaksi[1] . $persentase_transaksi[0] }}%) dibanding tahun
+                                        lalu)</small>
                                 </div>
                                 <div>
                                     <!-- select option -->
-                                    <select class="form-select ">
+                                    {{-- <select class="form-select ">
                                         <option value="2020">2020</option>
                                         <option value="2021">2021</option>
                                         <option value="2022">2022</option>
                                         <option value="2023" selected>2023</option>
-                                    </select>
+                                    </select> --}}
                                 </div>
 
                             </div>
@@ -130,7 +131,7 @@
                             <div id="totalSale" class="mt-6 d-flex justify-content-center"></div>
                             <div class="mt-4">
                                 <!-- list -->
-                                <ul class="list-unstyled mb-0">
+                                {{-- <ul class="list-unstyled mb-0">
                                     <li class="mb-2"><svg xmlns="http://www.w3.org/2000/svg" width="8" height="8"
                                             fill="currentColor" class="bi bi-circle-fill text-primary" viewBox="0 0 16 16">
                                             <circle cx="8" cy="8" r="8" />
@@ -147,7 +148,7 @@
                                         </svg> <span class="ms-1"><span class="text-dark">Terjual
                                                 600</span>
                                             (1%)</span></li>
-                                </ul>
+                                </ul> --}}
                             </div>
                         </div>
                     </div>
@@ -165,7 +166,8 @@
                         <div class="card-body">
                             <!-- table -->
                             <div class="table-responsive">
-                                <table class="table table-centered table-borderless text-nowrap table-hover w-100" id="transaksi">
+                                <table class="table table-centered table-borderless text-nowrap table-hover w-100"
+                                    id="transaksi">
                                     <thead class="bg-light">
                                         <tr>
                                             <th scope="col">ID Transaksi</th>
@@ -174,7 +176,6 @@
                                             <th scope="col">Total Transaksi</th>
                                             <th scope="col">Status</th>
                                             <th scope="col">Detail</th>
-
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -227,7 +228,7 @@
                         data: 'pembayaran[0].harga_jual',
                         render: function(data, type, row, meta) {
                             return $.fn.dataTable.render.number('.', ',', 0, 'Rp ', ',-')
-                                    .display(data);
+                                .display(data);
                         }
                     },
                     {
@@ -238,20 +239,25 @@
                             var nilai = '';
                             switch (data) {
                                 case '2':
-                                    nilai = `<span class="badge bg-light-danger text-dark-danger">Belum Diproses</span>`
+                                    nilai =
+                                        `<span class="badge bg-light-danger text-dark-danger">Belum Diproses</span>`
                                     break;
                                 case '3':
-                                    nilai = `<span class="badge bg-light-info text-dark-info">Pengemasan</span>`
+                                    nilai =
+                                        `<span class="badge bg-light-info text-dark-info">Pengemasan</span>`
                                     break;
                                 case '4':
-                                    nilai = `<span class="badge bg-light-warning text-dark-warning">Proses Pengiriman</span>`
+                                    nilai =
+                                        `<span class="badge bg-light-warning text-dark-warning">Proses Pengiriman</span>`
                                     break;
                                 case '5':
-                                    nilai = `<span class="badge bg-light-primary text-dark-primary">Selesai</span>`
+                                    nilai =
+                                        `<span class="badge bg-light-primary text-dark-primary">Selesai</span>`
                                     break;
-                                    
+
                                 default:
-                                    nilai = `<span class="badge bg-light-danger text-dark-danger">-</span>`
+                                    nilai =
+                                        `<span class="badge bg-light-danger text-dark-danger">-</span>`
                                     break;
                             }
                             // console.log(nilai);
@@ -269,9 +275,10 @@
                 ]
             });
 
-            $('#transaksi tbody').on('click', '.btn-detail', function(e) { 
+            // btn detail
+            $('#transaksi tbody').on('click', '.btn-detail', function(e) {
                 e.preventDefault();
-                
+
                 let data = table.row($(this).parents('tr')).data();
 
                 let url = `{{ route('admin.order.detail', ':id') }}`;
@@ -280,7 +287,245 @@
                 location.href = link
             });
 
+            // Chart Pendapatan
+            $.ajax({
+                type: "GET",
+                url: "{{ route('admin.index') }}",
+                dataType: "JSON",
+                data: {
+                    chart: 1
+                },
+                success: function(response) {
 
+                    // Option Pendapatan
+                    let pendapatan = {
+                        series: [{
+                            name: "Total Income",
+                            data: [31, 40, 28, 51, 42, 67, 100]
+                        }],
+                        labels: ["Jan", "Feb", "March", "April", "May", "Jun", "Jul", "Aug", "Sep",
+                            "Oct",
+                            "Nov", "Dec"
+                        ],
+                        chart: {
+                            height: 350,
+                            type: "area",
+                            toolbar: {
+                                show: !1
+                            }
+                        },
+                        dataLabels: {
+                            enabled: !1
+                        },
+                        markers: {
+                            size: 5,
+                            hover: {
+                                size: 6,
+                                sizeOffset: 3
+                            }
+                        },
+                        colors: ["#0aad0a"],
+                        stroke: {
+                            curve: "smooth",
+                            width: 2
+                        },
+                        grid: {
+                            borderColor: window.theme.gray300
+                        },
+                        xaxis: {
+                            labels: {
+                                show: !0,
+                                align: "right",
+                                minWidth: 0,
+                                maxWidth: 160,
+                                style: {
+                                    fontSize: "12px",
+                                    fontWeight: 400,
+                                    colors: [window.theme.gray600],
+                                    fontFamily: '"Inter", "sans-serif"'
+                                }
+                            },
+                            axisBorder: {
+                                show: !0,
+                                color: window.theme.gray300,
+                                height: 1,
+                                width: "100%",
+                                offsetX: 0,
+                                offsetY: 0
+                            },
+                            axisTicks: {
+                                show: !0,
+                                borderType: "solid",
+                                color: window.theme.gray300,
+                                height: 6,
+                                offsetX: 0,
+                                offsetY: 0
+                            }
+                        },
+                        legend: {
+                            position: "top",
+                            fontWeight: 600,
+                            color: window.theme.gray600,
+                            markers: {
+                                width: 8,
+                                height: 8,
+                                strokeWidth: 0,
+                                strokeColor: "#fff",
+                                fillColors: void 0,
+                                radius: 12,
+                                customHTML: void 0,
+                                onClick: void 0,
+                                offsetX: 0,
+                                offsetY: 0
+                            },
+                            labels: {
+                                colors: window.theme.gray600,
+                                useSeriesColors: !1
+                            }
+                        },
+                        yaxis: {
+                            labels: {
+                                formatter: function(e) {
+                                    return e + "k"
+                                },
+                                show: !0,
+                                align: "right",
+                                minWidth: 0,
+                                maxWidth: 160,
+                                style: {
+                                    fontSize: "12px",
+                                    fontWeight: 400,
+                                    colors: window.theme.gray600,
+                                    fontFamily: '"Inter", "sans-serif"'
+                                }
+                            }
+                        }
+                    }
+
+                    let bulan = []
+                    let totalTransaksi = []
+                    $.each(response.data, function(i, v) {
+                        bulan.push(v['bulan'])
+                        totalTransaksi.push(v['total'])
+                    });
+
+                    pendapatan['labels'] = bulan
+                    pendapatan.series[0].data = totalTransaksi
+
+                    // Init Chart Pendapatan
+                    var chartPendapatan = new ApexCharts(document.querySelector("#revenueChart"),
+                        pendapatan);
+                    chartPendapatan.render();
+                }
+            });
+
+            // Chart Penjualan
+            $.ajax({
+                type: "GET",
+                url: "{{ route('admin.index') }}",
+                dataType: "JSON",
+                data: {
+                    chart: 2
+                },
+                success: function(response) {
+
+                    // Option Pendapatan
+                    let penjualan = {
+                        series: [6e3, 1e3, 600],
+                        labels: ["Total Order", "Pengemasan", "Pengiriman", "Selesai"],
+                        colors: ["#0aad0a", "#ffc107", "#db3030"],
+                        chart: {
+                            type: "donut",
+                            height: 280
+                        },
+                        legend: {
+                            show: !1
+                        },
+                        dataLabels: {
+                            enabled: !1
+                        },
+                        plotOptions: {
+                            pie: {
+                                donut: {
+                                    size: "85%",
+                                    background: "transparent",
+                                    labels: {
+                                        show: !0,
+                                        name: {
+                                            show: !0,
+                                            fontSize: "22px",
+                                            fontFamily: '"Inter", "sans-serif"',
+                                            fontWeight: 600,
+                                            colors: [window.theme.gray600],
+                                            offsetY: -10,
+                                            formatter: function(e) {
+                                                return e
+                                            }
+                                        },
+                                        value: {
+                                            show: !0,
+                                            fontSize: "24px",
+                                            fontFamily: '"Inter", "sans-serif"',
+                                            fontWeight: 800,
+                                            colors: window.theme.gray800,
+                                            offsetY: 8,
+                                            formatter: function(e) {
+                                                return e
+                                            }
+                                        },
+                                        total: {
+                                            show: !0,
+                                            showAlways: !1,
+                                            label: "Total Penjualan",
+                                            fontSize: "16px",
+                                            fontFamily: '"Inter", "sans-serif"',
+                                            fontWeight: 400,
+                                            colors: window.theme.gray400,
+                                            formatter: function(e) {
+                                                return e.globals.seriesTotals.reduce((e, r) =>
+                                                    e + r, 0)
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        stroke: {
+                            width: 0
+                        },
+                        responsive: [{
+                            breakpoint: 1400,
+                            options: {
+                                chart: {
+                                    type: "donut",
+                                    width: 290,
+                                    height: 330
+                                }
+                            }
+                        }]
+                    }
+
+                    let status = []
+                    let total = []
+                    
+                    $.each(response.data, function(i, v) {
+                        status.push(v['status'])
+                        total.push(v['total'])
+                    });
+
+                    penjualan.series = total
+                    penjualan.labels = status
+
+                    // Init Chart Pendapatan
+                    var chartPenjualan = new ApexCharts(document.querySelector("#totalSale"),
+                        penjualan);
+                    chartPenjualan.render();
+
+                    console.log(response);
+                }
+            });
+
+            window.theme = theme
         });
     </script>
 @endsection
