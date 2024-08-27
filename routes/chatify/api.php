da7f29b7b2742454aa4b1907d2cecca2998881f5
firdausnanda <firdausnanda46@gmail.com>
2024-08-27 12:00:33 +0700

    fix: route api

--- routes/chatify/api.php
+++ routes/chatify/api.php
@@ -1,75 +1,76 @@
 <?php
 
+use App\Http\Controllers\Vendor\Chatify\Api\MessagesController;
 use Illuminate\Support\Facades\Route;
 
 /**
  * Authentication for pusher private channels
  */
-Route::post('/chat/auth', 'MessagesController@pusherAuth')->name('api.pusher.auth');
+Route::post('/chat/auth', [MessagesController::class, 'pusherAuth'])->name('api.pusher.auth');
 
 /**
  *  Fetch info for specific id [user/group]
  */
-Route::post('/idInfo', 'MessagesController@idFetchData')->name('api.idInfo');
+Route::post('/idInfo', [MessagesController::class, 'idFetchData'])->name('api.idInfo');
 
 /**
  * Send message route
  */
-Route::post('/sendMessage', 'MessagesController@send')->name('api.send.message');
+Route::post('/sendMessage', [MessagesController::class, 'send'])->name('api.send.message');
 
 /**
  * Fetch messages
  */
-Route::post('/fetchMessages', 'MessagesController@fetch')->name('api.fetch.messages');
+Route::post('/fetchMessages', [MessagesController::class, 'fetch'])->name('api.fetch.messages');
 
 /**
  * Download attachments route to create a downloadable links
  */
-Route::get('/download/{fileName}', 'MessagesController@download')->name('api.'.config('chatify.attachments.download_route_name'));
+Route::get('/download/{fileName}', [MessagesController::class, 'download'])->name('api.'.config('chatify.attachments.download_route_name'));
 
 /**
  * Make messages as seen
  */
-Route::post('/makeSeen', 'MessagesController@seen')->name('api.messages.seen');
+Route::post('/makeSeen', [MessagesController::class, 'seen'])->name('api.messages.seen');
 
 /**
  * Get contacts
  */
-Route::get('/getContacts', 'MessagesController@getContacts')->name('api.contacts.get');
+Route::get('/getContacts', [MessagesController::class, 'getContacts'])->name('api.contacts.get');
 
 /**
  * Star in favorite list
  */
-Route::post('/star', 'MessagesController@favorite')->name('api.star');
+Route::post('/star', [MessagesController::class, 'favorite'])->name('api.star');
 
 /**
  * get favorites list
  */
-Route::post('/favorites', 'MessagesController@getFavorites')->name('api.favorites');
+Route::post('/favorites', [MessagesController::class, 'getFavorites'])->name('api.favorites');
 
 /**
  * Search in messenger
  */
-Route::get('/search', 'MessagesController@search')->name('api.search');
+Route::get('/search', [MessagesController::class, 'search'])->name('api.search');
 
 /**
  * Get shared photos
  */
-Route::post('/shared', 'MessagesController@sharedPhotos')->name('api.shared');
+Route::post('/shared', [MessagesController::class, 'sharedPhotos'])->name('api.shared');
 
 /**
  * Delete Conversation
  */
-Route::post('/deleteConversation', 'MessagesController@deleteConversation')->name('api.conversation.delete');
+Route::post('/deleteConversation', [MessagesController::class, 'deleteConversation'])->name('api.conversation.delete');
 
 /**
  * Delete Conversation
  */
-Route::post('/updateSettings', 'MessagesController@updateSettings')->name('api.avatar.update');
+Route::post('/updateSettings', [MessagesController::class, 'updateSettings'])->name('api.avatar.update');
 
 /**
  * Set active status
  */
-Route::post('/setActiveStatus', 'MessagesController@setActiveStatus')->name('api.activeStatus.set');
+Route::post('/setActiveStatus', [MessagesController::class, 'setActiveStatus'])->name('api.activeStatus.set');
 
 
